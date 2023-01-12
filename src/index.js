87c5d5d435060e7887457e9b69ed10423100855a
Makcim80 <makcim80@mail.ru>
2023-01-12 16:57:23 +0300

    feat: implement task in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function toReadable (number) {
-  
+  const baseNumbersArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  const dozensArr = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+  if (number < 20) {  
+    return baseNumbersArr[number]
+  } else if (number < 100 && !(number % 10)) {
+    return dozensArr[Math.floor(number / 10)]
+  } else if (number < 100) {
+    return dozensArr[Math.floor(number / 10)] + ' ' + baseNumbersArr[(number % 10)]
+  } else if ( !(number % 100)) {
+    return baseNumbersArr[(Math.floor(number / 100))] + ' hundred'
+  } else if (number % 100 < 20) {
+    return baseNumbersArr[Math.floor(number / 100)] + ' hundred ' + baseNumbersArr[(number % 100)]
+  } else if (!(number % 10)) {
+    return baseNumbersArr[Math.floor(number / 100)] + ' hundred ' + dozensArr[Math.floor((number % 100) / 10)]
+  } else {
+    return baseNumbersArr[Math.floor(number / 100)] + ' hundred ' + dozensArr[Math.floor((number % 100) / 10)] + ' ' + baseNumbersArr[(number % 10)]
+  }
 }
